b17d6f957b534e11cc6a978fd1a7023432626e8f
AbdulazizRus <coderdim@gmail.com>
2024-09-01 13:05:28 +0500

    changes

---
+++ src/app/api/users/[userId]/followers/route.ts
@@ -0,0 +1,53 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { FollowerInfo } from "@/lib/types";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        followers: {
+          where: {
+            followerId: loggedInUser.id,
+          },
+          select: {
+            followerId: true,
+          },
+        },
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const data: FollowerInfo = {
+      followers: user._count.followers,
+      isFollowedByUser: !!user.followers.length,
+    };
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
