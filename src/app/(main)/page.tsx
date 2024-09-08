9e751e2847ee6cdcc383b7fa9d3bbcaf73063948
AbdulazizRus <coderdim@gmail.com>
2024-09-08 14:14:15 +0500

    changes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,7 @@
 import TrendsSidebar from "@/components/TrendsSidebar";
 import PostEditor from "@/components/posts/editor/PostEditor";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFedd from "./FollowingFeed";
 import ForYouFeed from "./ForYouFeed";
 
 export default function Home() {
@@ -7,7 +9,18 @@ export default function Home() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFedd />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
