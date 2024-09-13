0ca4bf093d03868b6bb8a92dde79499381e20607
AbdulazizRus <coderdim@gmail.com>
2024-09-13 16:00:47 +0500

    changes

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,7 +1,13 @@
 import { validateRequest } from "@/auth";
+import FollowButton from "@/components/FollowButton";
+import FollowerCount from "@/components/FollowerCount";
 import TrendsSidebar from "@/components/TrendsSidebar";
+import UserAvatar from "@/components/UserAvatar";
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
-import { getUserDataSelect, UserData } from "@/lib/types";
+import { FollowerInfo, UserData, getUserDataSelect } from "@/lib/types";
+import { formatNumber } from "@/lib/utils";
+import { formatDate } from "date-fns";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { cache } from "react";
@@ -55,7 +61,9 @@ export default async function Page({ params: { username } }: PageProps) {
 
   return (
     <main className="flex w-full min-w-0 gap-5">
-      <div className="w-full min-w-0 space-y-5"></div>
+      <div className="w-full min-w-0 space-y-5">
+        <UserProfile user={user} loggedInUserId={loggedInUser.id} />
+      </div>
       <TrendsSidebar />
     </main>
   );
@@ -67,5 +75,43 @@ interface UserProfileProps {
 }
 
 async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
-    
+  const followerInfo: FollowerInfo = {
+    followers: user._count.followers,
+    isFollowedByUser: user.followers.some(
+      ({ followerId }) => followerId === loggedInUserId,
+    ),
+  };
+
+  return (
+    <div className="h-fit w-full space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <UserAvatar
+        avatarUrl={user.avatarUrl}
+        size={250}
+        className="mx-auto size-full max-h-60 max-w-60 rounded-full"
+      />
+      <div className="flex flex-wrap gap-3 sm:flex-nowrap">
+        <div className="me-auto space-y-3">
+          <div>
+            <h1 className="text-3xl font-bold">{user.displayName}</h1>
+            <div className="text-muted-foreground">@{user.username}</div>
+          </div>
+          <div>Member since {formatDate(user.createdAt, "MMM d, yyyy")}</div>
+          <div className="flex items-center gap-3">
+            <span>
+              Posts:{" "}
+              <span className="font-semibold">
+                {formatNumber(user._count.posts)}
+              </span>
+            </span>
+            <FollowerCount userId={user.id} initialState={followerInfo} />
+          </div>
+        </div>
+        {user.id === loggedInUserId ? (
+          <Button>Edit profile</Button>
+        ) : (
+          <FollowButton userId={user.id} initialState={followerInfo} />
+        )}
+      </div>
+    </div>
+  );
 }
